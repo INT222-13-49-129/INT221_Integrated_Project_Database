318f16d3bc0167e7be944f61c17c1c2dc1de3910
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-09-01 23:59:06 +0700

    [Update] change Ingredians to Ingredients

--- INSERT example.sql
+++ INSERT example.sql
@@ -2,9 +2,9 @@ INSERT INTO `cfandb`.`user` (`Userid`, `Username`, `Email`, `Password`, `Firstna
 INSERT INTO `cfandb`.`user` (`Userid`, `Username`, `Email`, `Password`, `Firstname`, `Lastname`, `DoB`, `Gender`, `Weight`, `Height`, `Status`, `Image`) VALUES ('102', 'Ly', 'Ly@gmail.com', 'holycrab', 'Jo', 'Seph', '1968-06-15', 'F', '85', '195', 'NORMAL', 'aaa.jpg');
 INSERT INTO `cfandb`.`user` (`Userid`, `Username`, `Email`, `Password`, `Firstname`, `Lastname`, `DoB`, `Gender`, `Weight`, `Height`, `Status`, `Image`) VALUES ('103', 'Crab', 'Crag@gmail.com', 'holycrab', 'Jo', 'Taro', '1991-09-13', 'M', '80', '190', 'NORMAL', 'aaa.jpg');
 
-INSERT INTO `cfandb`.`ingredians` (`Ingradiansid`, `Ingradianname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingrediantype`) VALUES ('201', 'Tomato', '50', 'gram', 'Red vegetable', 'Vegetable');
-INSERT INTO `cfandb`.`ingredians` (`Ingradiansid`, `Ingradianname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingrediantype`) VALUES ('202', 'Potato', '100', 'gram', 'Brown vegetable', 'Vegetable');
-INSERT INTO `cfandb`.`ingredians` (`Ingradiansid`, `Ingradianname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingrediantype`) VALUES ('203', 'Onion', '50', 'gram', 'Cream vegetable', 'Vegetable');
+INSERT INTO `cfandb`.`Ingredients` (`Ingredientsid`, `Ingredientsname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingredientstype`) VALUES ('201', 'Tomato', '50', 'gram', 'Red vegetable', 'Vegetable');
+INSERT INTO `cfandb`.`Ingredients` (`Ingredientsid`, `Ingredientsname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingredientstype`) VALUES ('202', 'Potato', '100', 'gram', 'Brown vegetable', 'Vegetable');
+INSERT INTO `cfandb`.`Ingredients` (`Ingredientsid`, `Ingredientsname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingredientstype`) VALUES ('203', 'Onion', '50', 'gram', 'Cream vegetable', 'Vegetable');
 
 INSERT INTO `cfandb`.`foodtype` (`Foodtypeid`, `Typename`) VALUES ('301', 'boiled');
 INSERT INTO `cfandb`.`foodtype` (`Foodtypeid`, `Typename`) VALUES ('302', 'fried');
@@ -18,9 +18,9 @@ INSERT INTO `cfandb`.`meal` (`Mealid`, `Mealtime`, `Datemeal`, `Totalkcal`, `Use
 INSERT INTO `cfandb`.`meal` (`Mealid`, `Mealtime`, `Datemeal`, `Totalkcal`, `User_Userid`) VALUES ('502', 'Dinner', '2021-03-22', '600', '102');
 INSERT INTO `cfandb`.`meal` (`Mealid`, `Mealtime`, `Datemeal`, `Totalkcal`, `User_Userid`) VALUES ('503', 'Dinner', '2021-03-23', '450', '103');
 
-INSERT INTO `cfandb`.`foodmenu_has_ingredians` (`Foodmenu_Foodmenuid`, `Ingredians_Ingradiansid`, `Totalunit`, `Totalkcal`) VALUES ('401', '201', '5', '212');
-INSERT INTO `cfandb`.`foodmenu_has_ingredians` (`Foodmenu_Foodmenuid`, `Ingredians_Ingradiansid`, `Totalunit`, `Totalkcal`) VALUES ('402', '202', '10', '309');
-INSERT INTO `cfandb`.`foodmenu_has_ingredians` (`Foodmenu_Foodmenuid`, `Ingredians_Ingradiansid`, `Totalunit`, `Totalkcal`) VALUES ('403', '203', '15', '150');
+INSERT INTO `cfandb`.`foodmenu_has_Ingredients` (`Foodmenu_Foodmenuid`, `Ingredients_Ingredientsid`, `Totalunit`, `Totalkcal`) VALUES ('401', '201', '5', '212');
+INSERT INTO `cfandb`.`foodmenu_has_Ingredients` (`Foodmenu_Foodmenuid`, `Ingredients_Ingredientsid`, `Totalunit`, `Totalkcal`) VALUES ('402', '202', '10', '309');
+INSERT INTO `cfandb`.`foodmenu_has_Ingredients` (`Foodmenu_Foodmenuid`, `Ingredients_Ingredientsid`, `Totalunit`, `Totalkcal`) VALUES ('403', '203', '15', '150');
 
 INSERT INTO `cfandb`.`meal_has_foodmenu` (`Meal_Mealid`, `Foodmenu_Foodmenuid`, `Totaldish`, `Totalkcal`) VALUES ('501', '401', '2', '3500');
 INSERT INTO `cfandb`.`meal_has_foodmenu` (`Meal_Mealid`, `Foodmenu_Foodmenuid`, `Totaldish`, `Totalkcal`) VALUES ('502', '402', '2', '4200');
@@ -33,9 +33,9 @@ INSERT INTO `cfandb`.`request` (`Requestid`, `Status`, `User_Userid`) VALUES ('6
 INSERT INTO `cfandb`.`request` (`Requestid`, `Status`, `User_Userid`) VALUES ('605', 'WAIT', '101');
 INSERT INTO `cfandb`.`request` (`Requestid`, `Status`, `User_Userid`) VALUES ('606', 'WAIT', '102');
 
-INSERT INTO `cfandb`.`ingrediansreq` (`Ingradianname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingrediantype`, `Request_Requestid`) VALUES ('Tomato', '212', 'g', 'gram', 'Vegetable', '601');
-INSERT INTO `cfandb`.`ingrediansreq` (`Ingradianname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingrediantype`, `Request_Requestid`) VALUES ('Potato', '309', 'g', 'gram', 'Vegetable', '602');
-INSERT INTO `cfandb`.`ingrediansreq` (`Ingradianname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingrediantype`, `Request_Requestid`) VALUES ('Onion', '150', 'g', 'gram', 'Vegetable', '603');
+INSERT INTO `cfandb`.`Ingredientsreq` (`Ingredientsname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingredientstype`, `Request_Requestid`) VALUES ('Tomato', '212', 'g', 'gram', 'Vegetable', '601');
+INSERT INTO `cfandb`.`Ingredientsreq` (`Ingredientsname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingredientstype`, `Request_Requestid`) VALUES ('Potato', '309', 'g', 'gram', 'Vegetable', '602');
+INSERT INTO `cfandb`.`Ingredientsreq` (`Ingredientsname`, `kcalpunit`, `Unit`, `Descriptionunit`, `Ingredientstype`, `Request_Requestid`) VALUES ('Onion', '150', 'g', 'gram', 'Vegetable', '603');
 
 INSERT INTO `cfandb`.`foodtypereq` (`Typename`, `Request_Requestid`) VALUES ('fried', '604');
 INSERT INTO `cfandb`.`foodtypereq` (`Typename`, `Request_Requestid`) VALUES ('boiled', '605');
