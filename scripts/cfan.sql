cc420501a0a24427187d3cc2f6ab7a178260c005
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-09-22 16:49:41 +0700

    [Add] cfan.sql

--- scripts/cfan.sql
+++ scripts/cfan.sql
@@ -16,6 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `foodmenu`
+--
+
+DROP TABLE IF EXISTS `foodmenu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `foodmenu` (
+  `Foodmenuid` int NOT NULL AUTO_INCREMENT,
+  `Foodname` varchar(50) NOT NULL,
+  `Totalkcal` int DEFAULT NULL,
+  `Image` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Foodmenustatus` enum('PUBLISH','PERSONAL') NOT NULL DEFAULT 'PERSONAL',
+  `Foodtype_Foodtypeid` int NOT NULL,
+  `User_Userid` int DEFAULT NULL,
+  PRIMARY KEY (`Foodmenuid`),
+  KEY `fk_Foodmenu_Foodtype1_idx` (`Foodtype_Foodtypeid`),
+  KEY `fk_Foodmenu_User1_idx` (`User_Userid`),
+  CONSTRAINT `fk_Foodmenu_Foodtype1` FOREIGN KEY (`Foodtype_Foodtypeid`) REFERENCES `foodtype` (`Foodtypeid`),
+  CONSTRAINT `fk_Foodmenu_User1` FOREIGN KEY (`User_Userid`) REFERENCES `user` (`Userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=404 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `foodmenu`
 --
 
@@ -24,6 +48,26 @@ LOCK TABLES `foodmenu` WRITE;
 INSERT INTO `foodmenu` VALUES (401,'Pasta',212,'Double...','Italian food','PUBLISH',301,101),(402,'Pizza',309,'Triple...','Italian food','PERSONAL',302,102),(403,'Panzenella',150,'Quad...','Italian food','PUBLISH',303,103);
 /*!40000 ALTER TABLE `foodmenu` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `foodmenu_has_ingredients`
+--
+
+DROP TABLE IF EXISTS `foodmenu_has_ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `foodmenu_has_ingredients` (
+  `Foodmenu_Foodmenuid` int NOT NULL,
+  `Ingredients_Ingredientsid` int NOT NULL,
+  `Totalunit` int DEFAULT NULL,
+  `Totalkcal` int DEFAULT NULL,
+  PRIMARY KEY (`Foodmenu_Foodmenuid`,`Ingredients_Ingredientsid`),
+  KEY `fk_Foodmenu_has_Ingredients_Ingredients1_idx` (`Ingredients_Ingredientsid`),
+  KEY `fk_Foodmenu_has_Ingredients_Foodmenu1_idx` (`Foodmenu_Foodmenuid`),
+  CONSTRAINT `fk_Foodmenu_has_Ingredients_Foodmenu1` FOREIGN KEY (`Foodmenu_Foodmenuid`) REFERENCES `foodmenu` (`Foodmenuid`),
+  CONSTRAINT `fk_Foodmenu_has_Ingredients_Ingredients1` FOREIGN KEY (`Ingredients_Ingredientsid`) REFERENCES `ingredients` (`Ingredientsid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `foodmenu_has_ingredients`
@@ -36,6 +80,21 @@ INSERT INTO `foodmenu_has_ingredients` VALUES (401,201,5,212),(402,202,10,309),(
 UNLOCK TABLES;
 
 --
+-- Table structure for table `foodtype`
+--
+
+DROP TABLE IF EXISTS `foodtype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `foodtype` (
+  `Foodtypeid` int NOT NULL AUTO_INCREMENT,
+  `Typename` varchar(50) NOT NULL,
+  PRIMARY KEY (`Foodtypeid`),
+  UNIQUE KEY `Typename_UNIQUE` (`Typename`)
+) ENGINE=InnoDB AUTO_INCREMENT=304 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `foodtype`
 --
 
@@ -44,6 +103,22 @@ LOCK TABLES `foodtype` WRITE;
 INSERT INTO `foodtype` VALUES (301,'boiled'),(302,'fried'),(303,'puff');
 /*!40000 ALTER TABLE `foodtype` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `foodtypereq`
+--
+
+DROP TABLE IF EXISTS `foodtypereq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `foodtypereq` (
+  `Typename` varchar(50) NOT NULL,
+  `Request_Requestid` int NOT NULL,
+  PRIMARY KEY (`Request_Requestid`),
+  UNIQUE KEY `Typename_UNIQUE` (`Typename`),
+  CONSTRAINT `fk_Foodtype_req_Request1` FOREIGN KEY (`Request_Requestid`) REFERENCES `request` (`Requestid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `foodtypereq`
@@ -56,6 +131,25 @@ INSERT INTO `foodtypereq` VALUES ('boiled',605),('fried',604),('puff',606);
 UNLOCK TABLES;
 
 --
+-- Table structure for table `ingredients`
+--
+
+DROP TABLE IF EXISTS `ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ingredients` (
+  `Ingredientsid` int NOT NULL AUTO_INCREMENT,
+  `Ingredientsname` varchar(50) NOT NULL,
+  `kcalpunit` int DEFAULT NULL,
+  `Unit` varchar(50) DEFAULT NULL,
+  `Descriptionunit` varchar(255) DEFAULT NULL,
+  `Ingredientstype` enum('Oil','Carb','Meat','Vegetable','Fruit','Condiment') NOT NULL,
+  PRIMARY KEY (`Ingredientsid`),
+  UNIQUE KEY `Ingredientsname_UNIQUE` (`Ingredientsname`)
+) ENGINE=InnoDB AUTO_INCREMENT=204 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `ingredients`
 --
 
@@ -64,6 +158,26 @@ LOCK TABLES `ingredients` WRITE;
 INSERT INTO `ingredients` VALUES (201,'Tomato',50,'gram','Red vegetable','Vegetable'),(202,'Potato',100,'gram','Brown vegetable','Vegetable'),(203,'Onion',50,'gram','Cream vegetable','Vegetable');
 /*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredientsreq`
+--
+
+DROP TABLE IF EXISTS `ingredientsreq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ingredientsreq` (
+  `Ingredientsname` varchar(50) NOT NULL,
+  `kcalpunit` int DEFAULT NULL,
+  `Unit` varchar(50) DEFAULT NULL,
+  `Descriptionunit` varchar(255) DEFAULT NULL,
+  `Ingredientstype` enum('Oil','Carb','Meat','Vegetable','Fruit','Condiment') DEFAULT NULL,
+  `Request_Requestid` int NOT NULL,
+  PRIMARY KEY (`Request_Requestid`),
+  UNIQUE KEY `Ingredientsname_UNIQUE` (`Ingredientsname`),
+  CONSTRAINT `fk_Ingredientsreq_Request1` FOREIGN KEY (`Request_Requestid`) REFERENCES `request` (`Requestid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `ingredientsreq`
@@ -76,6 +190,24 @@ INSERT INTO `ingredientsreq` VALUES ('Tomato',212,'g','gram','Vegetable',601),('
 UNLOCK TABLES;
 
 --
+-- Table structure for table `jwtblacklist`
+--
+
+DROP TABLE IF EXISTS `jwtblacklist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jwtblacklist` (
+  `Jwtblacklistid` int NOT NULL AUTO_INCREMENT,
+  `token` varchar(255) DEFAULT NULL,
+  `exp` datetime DEFAULT NULL,
+  `User_Userid` int NOT NULL,
+  PRIMARY KEY (`Jwtblacklistid`),
+  KEY `fk_Jwtblacklist_User1_idx` (`User_Userid`),
+  CONSTRAINT `fk_Jwtblacklist_User1` FOREIGN KEY (`User_Userid`) REFERENCES `user` (`Userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=721 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `jwtblacklist`
 --
 
@@ -84,6 +216,25 @@ LOCK TABLES `jwtblacklist` WRITE;
 INSERT INTO `jwtblacklist` VALUES (707,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzA3MzE3fQ.ghH4h3vm5KtrFeqvezLWnx3Yb1W-LSjZaemFqQitttE','2021-09-21 17:41:57',111),(708,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzA5MzAwfQ.xiHbwadUJEGJNFxO-ysMNeraxjFMI9H14deS_ywzKf0','2021-09-21 18:15:00',111),(709,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzEzMjc4fQ.ZwmGJGLI8R6e3knQAAnyQbz5oyqQezDj9i-61EzhxDE','2021-09-21 19:21:18',111),(710,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzEzMzQ2fQ._l8b9jWJ-8f0_2soe8voiOJRUQ2EYA4gJZ-SnGPehIQ','2021-09-21 19:22:26',111),(711,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzE2NjAyfQ.d3iTmFJ8_lYLmtP4NK7beTIZhThzr23PVZs5U5K2BIA','2021-09-21 20:16:42',111),(712,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzE2OTI0fQ.-oBE0xU2h2msfuLtHj4AkFuYfTHMaieFdzd6i2eOcvc','2021-09-21 20:22:04',111),(713,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzE2OTcyfQ.tVuRhul82ahuhjSnCgvzyxSAsbuSuFT-tkx31-8238g','2021-09-21 20:22:52',111),(714,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzE3MDQxfQ.SjyPL-ZA4AimuTBtUJUD7V4pjwtTdfLcOUhWaw5s8LQ','2021-09-21 20:24:01',111),(715,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzE3MTcwfQ.Eqjy5ti7otbx5QZQYgM9KovIBl3HUqKiGlRaK8_T1ck','2021-09-21 20:26:10',111),(716,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzE3MjIxfQ.B_Di5PffQMyOf8SGqnxAkehE7FkTY0OIr83IxE05yME','2021-09-21 20:27:01',111),(717,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzE5NTg2fQ.g7rhHtjebifrzllrJggRtEv_HAYzU3HBXPpfJRff_ZU','2021-09-21 21:06:26',111),(718,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzE5Nzc5fQ.-p0uC_wGzf27p-5gDQBHxLUVViUViDII7Tdceqsou-0','2021-09-21 21:09:39',111),(719,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzI1NDQxfQ.yIAuGgfefUP3WiWHg0Dp5dKZf7h8y5Ixbu3U6d7LpWc','2021-09-22 22:44:01',111),(720,'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcmluY2lwYWwiOjExMSwicm9sZSI6Ik5PUk1BTCIsImlzcyI6ImNmYW5CYWNrZW5kIiwiZXhwIjoxNjMyMzI1NDg5fQ.SkSWwEm8PGDBhciCPphrj9EMltTxfYGzMZcy8GXr98E','2021-09-22 22:44:49',111);
 /*!40000 ALTER TABLE `jwtblacklist` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `meal`
+--
+
+DROP TABLE IF EXISTS `meal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `meal` (
+  `Mealid` int NOT NULL AUTO_INCREMENT,
+  `Mealtime` enum('Breakfast','Lunch','Dinner','Lightmeal') DEFAULT NULL,
+  `Datemeal` date DEFAULT NULL,
+  `Totalkcal` int DEFAULT NULL,
+  `User_Userid` int NOT NULL,
+  PRIMARY KEY (`Mealid`),
+  KEY `fk_Meal_User1_idx` (`User_Userid`),
+  CONSTRAINT `fk_Meal_User1` FOREIGN KEY (`User_Userid`) REFERENCES `user` (`Userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=504 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `meal`
@@ -96,6 +247,26 @@ INSERT INTO `meal` VALUES (501,'Lunch','2021-03-21',550,101),(502,'Dinner','2021
 UNLOCK TABLES;
 
 --
+-- Table structure for table `meal_has_foodmenu`
+--
+
+DROP TABLE IF EXISTS `meal_has_foodmenu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `meal_has_foodmenu` (
+  `Meal_Mealid` int NOT NULL,
+  `Foodmenu_Foodmenuid` int NOT NULL,
+  `Totaldish` int DEFAULT NULL,
+  `Totalkcal` int DEFAULT NULL,
+  PRIMARY KEY (`Meal_Mealid`,`Foodmenu_Foodmenuid`),
+  KEY `fk_Meal_has_Foodmenu_Foodmenu1_idx` (`Foodmenu_Foodmenuid`),
+  KEY `fk_Meal_has_Foodmenu_Meal1_idx` (`Meal_Mealid`),
+  CONSTRAINT `fk_Meal_has_Foodmenu_Foodmenu1` FOREIGN KEY (`Foodmenu_Foodmenuid`) REFERENCES `foodmenu` (`Foodmenuid`),
+  CONSTRAINT `fk_Meal_has_Foodmenu_Meal1` FOREIGN KEY (`Meal_Mealid`) REFERENCES `meal` (`Mealid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `meal_has_foodmenu`
 --
 
@@ -104,6 +275,25 @@ LOCK TABLES `meal_has_foodmenu` WRITE;
 INSERT INTO `meal_has_foodmenu` VALUES (501,401,2,3500),(502,402,2,4200),(503,403,2,2500);
 /*!40000 ALTER TABLE `meal_has_foodmenu` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `pin`
+--
+
+DROP TABLE IF EXISTS `pin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pin` (
+  `Pinid` int NOT NULL AUTO_INCREMENT,
+  `Email` varchar(50) DEFAULT NULL,
+  `Pincode` varchar(255) DEFAULT NULL,
+  `exp` datetime DEFAULT NULL,
+  `User_Userid` int NOT NULL,
+  PRIMARY KEY (`Pinid`),
+  KEY `fk_Pin_User1_idx` (`User_Userid`),
+  CONSTRAINT `fk_Pin_User1` FOREIGN KEY (`User_Userid`) REFERENCES `user` (`Userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `pin`
@@ -115,6 +305,23 @@ LOCK TABLES `pin` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `request`
+--
+
+DROP TABLE IF EXISTS `request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `request` (
+  `Requestid` int NOT NULL AUTO_INCREMENT,
+  `Status` enum('WAIT','APPROVE','DISAPPROVED') NOT NULL DEFAULT 'WAIT',
+  `User_Userid` int NOT NULL,
+  PRIMARY KEY (`Requestid`),
+  KEY `fk_Request_User1_idx` (`User_Userid`),
+  CONSTRAINT `fk_Request_User1` FOREIGN KEY (`User_Userid`) REFERENCES `user` (`Userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=607 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `request`
 --
 
@@ -123,6 +330,32 @@ LOCK TABLES `request` WRITE;
 INSERT INTO `request` VALUES (601,'WAIT',101),(602,'WAIT',102),(603,'WAIT',101),(604,'WAIT',102),(605,'WAIT',101),(606,'WAIT',102);
 /*!40000 ALTER TABLE `request` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `Userid` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Firstname` varchar(50) NOT NULL,
+  `Lastname` varchar(50) DEFAULT NULL,
+  `DoB` date DEFAULT NULL,
+  `Gender` enum('M','F') DEFAULT NULL,
+  `Weight` decimal(5,2) DEFAULT NULL,
+  `Height` decimal(5,2) DEFAULT NULL,
+  `Status` enum('TBC','NORMAL','ADMIN') NOT NULL DEFAULT 'TBC',
+  `Image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Userid`),
+  UNIQUE KEY `Username_UNIQUE` (`Username`),
+  UNIQUE KEY `Email_UNIQUE` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=112 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
@@ -143,4 +376,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-09-22 16:40:16
+-- Dump completed on 2021-09-22 16:48:57
